33f9ac85c77ce04e3dde70bf7545e6bd674e235a
Caue Garcia <cauerg7@outlook.com>
2024-03-28 11:56:15 -0300

    position entity created.

---
+++ src/main/java/com/dev/entities/Position.java
@@ -0,0 +1,71 @@
+package com.dev.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "positions")
+public class Position {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String position;
+	private Double salary;
+	
+	public Position() {
+	}
+
+	public Position(Long id, String position, Double salary) {
+		super();
+		this.id = id;
+		this.position = position;
+		this.salary = salary;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		return Objects.equals(id, other.id);
+	}
+}
