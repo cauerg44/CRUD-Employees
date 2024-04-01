95fdcf38632dc3ac375d5e3ea104de86f527fb80
Caue Garcia <cauerg7@outlook.com>
2024-04-01 11:01:21 -0300

    BCrypt password encoded.

--- src/main/java/com/dev/ProjectApplication.java
+++ src/main/java/com/dev/ProjectApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
-
+public class ProjectApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
-
 }
