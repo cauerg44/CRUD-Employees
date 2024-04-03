bd4a774725f4472f58684a9a12d5ae7b676f4aff
Caue Garcia <cauerg7@outlook.com>
2024-04-03 14:07:53 -0300

    Implementing tests for controllers with MockMVC.

--- src/main/java/com/dev/controllers/DepartmentController.java
+++ src/main/java/com/dev/controllers/DepartmentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,6 @@ public class DepartmentController {
 	@Autowired
 	private DepartmentService departmentService;
 	
-	@PreAuthorize("hasAnyRole('ROLE_EMPLOYEE', 'ROLE_CEO')")
 	@GetMapping
 	public ResponseEntity<List<DepartmentDTO>> findAll() {
 		List<DepartmentDTO> list = departmentService.findAll();
